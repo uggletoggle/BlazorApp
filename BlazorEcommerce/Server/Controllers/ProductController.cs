0cb60bcf21ff7010a62521de80176536452f8e83
carlos.boyanosky <carlos.boyanosky@cdev.global>
2022-06-21 19:15:38 -0400

    Ecommerce Blazor

---
+++ BlazorEcommerce/Server/Controllers/ProductController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorEcommerce.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private static List<Product> Products = new List<Product>
+        {
+            new Product
+            {
+                Id = 1,
+                Title = "Producto Medico",
+                Description = "Tomografo xxl",
+                ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/Wilcox.jpg/150px-Wilcox.jpg",
+                Price = 9.99m
+            },
+            new Product
+            {
+                Id = 2,
+                Title = "Producto Agricola",
+                Description = "Tractor usado",
+                ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/f/f7/VioletNeuron.png",
+                Price = 6.99m
+            },
+            new Product
+            {
+                Id = 3,
+                Title = "Producto Farmacia",
+                Description = "Ciclopramida",
+                ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Dulce_Pi%C3%B1onate%2C_Isla_Margarita.jpg/1280px-Dulce_Pi%C3%B1onate%2C_Isla_Margarita.jpg",
+                Price = 3.99m
+            },
+
+        };
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(Products);
+        }
+    }
+}
---
+++ BlazorEcommerce/Server/Program.cs
@@ -0,0 +1,40 @@
+global using BlazorEcommerce.Shared;
+using Microsoft.AspNetCore.ResponseCompression;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseSwaggerUI();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+}
+
+app.UseSwagger();
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+
+app.MapRazorPages();
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+app.Run();
